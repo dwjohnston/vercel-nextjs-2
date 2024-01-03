8eb720e306f35bf006cca23a2733d8847e4aced1
David Johnston <david@blacksheepcode.com>
2024-01-03 12:24:16 +1100

    Add skeleton.

--- src/components/RickAndMorty/RickAndMortyCharacter.tsx
+++ src/components/RickAndMorty/RickAndMortyCharacter.tsx
@@ -1,6 +1,7 @@
 import { gql, useQuery } from "@apollo/client";
 import { Box, Heading, Text } from "@chakra-ui/react";
 import Image from "next/image";
+import { Skeleton, SkeletonCircle, SkeletonText, Stack } from '@chakra-ui/react'
 
 const GET_SINGLE_CHARACTER = gql`
 query SingleCharacter($id: ID!) {
@@ -38,27 +39,32 @@ export function SingleCharacter(props: {
     console.log(data);
 
 
-    const character = data?.character; 
-     
-    if(!character) {
-        return null; 
-    }
-
+    const character = data?.character;
 
     return <Box>
-        <Heading fontSize={"2xl"} margin="1em 0">{character.name}</Heading>
-        <Image src={character.image} alt={character.name} width={300} height={300} style={{"margin": "0 auto 1em"}} />
+        <Skeleton isLoaded={!!character}>
+            <Heading fontSize={"2xl"} margin="1em 0">{character?.name}</Heading>
+        </Skeleton>
+        <Skeleton isLoaded={!!character}>
+            <Image src={character?.image} alt={character?.name} width={300} height={300} style={{ "margin": "0 auto 1em" }} />
+        </Skeleton>
         <dl>
-            <dt>Status</dt>
-            <dd>{character.status}</dd>
-
-            <dt>Species</dt>
-            <dd>{character.species}</dd>
-
-            <dt>Origin</dt>
-            <dd>{character.origin.name}</dd>
-            <dt>Last Known Location</dt>
-            <dd>{character.location.name}</dd>
+            <Skeleton isLoaded={!!character}>
+                <dt>Status</dt>
+                <dd>{character?.status}</dd>
+            </Skeleton>
+            <Skeleton isLoaded={!!character}>
+                <dt>Species</dt>
+                <dd>{character?.species}</dd>
+            </Skeleton>
+            <Skeleton isLoaded={!!character}>
+                <dt>Origin</dt>
+                <dd>{character?.origin.name}</dd>
+            </Skeleton>
+            <Skeleton isLoaded={!!character}>
+                <dt>Last Known Location</dt>
+                <dd>{character?.location.name}</dd>
+            </Skeleton>
         </dl>
     </Box>
 }
